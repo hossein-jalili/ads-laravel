5d3c47a89c25d63f3a8af3f481ab1c1c52b3984f
dev2 <dev2@webteamwork.ca>
2025-07-11 13:57:01 +0330

    Refactor: Clean up and optimize the code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\ShippingCostService;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -13,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(ShippingCostService::class);
+
     }
 
     /**
